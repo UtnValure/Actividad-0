bef698231a6ca4bfc0febb29d866657efe7c6ea2
UtnValure <vlaure@alu.frlp.utn.edu.ar>
2025-04-10 19:57:15 -0300

    [Parte 2.3: Activando modelos]

--- DjangoProject/polls/models.py
+++ DjangoProject/polls/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class.pregunta(models.Model):
-    pregunta = models.charField(max_lenght=200)
+class pregunta(models.Model):
+    pregunta = models.CharField(max_length=200)
     pub_date = models.DateTimeField('fecha de publicacion')
 
-class.opcion(models.Model):
+class opcion(models.Model):
     pregunta = models.ForeignKey(pregunta, on_delete=models.CASCADE)
-    opcion = models.CharField(max_lenght=200)
+    opcion = models.CharField(max_length=200)
     votos = models.IntegerField(default=0)

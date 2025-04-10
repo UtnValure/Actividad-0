2b4d34fc8cce5457de85aa6d8a3bf8084ee3b2ed
UtnValure <vlaure@alu.frlp.utn.edu.ar>
2025-04-10 19:42:07 -0300

    [Parte 2.2: Creacion de modelos BBDD]

--- DjangoProject/polls/models.py
+++ DjangoProject/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class.pregunta(models.Model):
+    pregunta = models.charField(max_lenght=200)
+    pub_date = models.DateTimeField('fecha de publicacion')
+
+class.opcion(models.Model):
+    pregunta = models.ForeignKey(pregunta, on_delete=models.CASCADE)
+    opcion = models.CharField(max_lenght=200)
+    votos = models.IntegerField(default=0)

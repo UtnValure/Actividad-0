c3c44f99d321e6e18709ad62f0cfaeda05cc55de
UtnValure <vlaure@alu.frlp.utn.edu.ar>
2025-04-10 20:29:18 -0300

    [Parte 2.4: Jugando con la API ]

--- DjangoProject/polls/models.py
+++ DjangoProject/polls/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 class pregunta(models.Model):
     pregunta = models.CharField(max_length=200)
     pub_date = models.DateTimeField('fecha de publicacion')
+    def __str__(self):
+        return self.pregunta
+    def fue_publicada_recientemente(self):
+        return self.pub_date >= timezone.now() - timezone.timedelta(days=1)
 
 class opcion(models.Model):
     pregunta = models.ForeignKey(pregunta, on_delete=models.CASCADE)
     opcion = models.CharField(max_length=200)
     votos = models.IntegerField(default=0)
+    def __str__ (self):
+        return self.opcion

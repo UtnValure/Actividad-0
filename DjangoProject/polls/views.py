a1e370ce3c26cf59c5ede0c68c7fd5cbae51573b
UtnValure <vlaure@alu.frlp.utn.edu.ar>
2025-04-11 16:48:57 -0300

    [Parte 3.3: Excepción 404 y retoques a las plantillas ]

--- DjangoProject/polls/views.py
+++ DjangoProject/polls/views.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import pregunta
 from django.template import loader
 
 def index(request):
     ultimas_preguntas = pregunta.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"ultimas_preguntas": ultimas_preguntas}
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
+    # render usa ( request, plantilla, diccionario opcional)
     
 def results(request, pregunta_id):
     response = "Estas viendo los resultados de la pregunta %s."
     return HttpResponse(response % pregunta_id)
 
 def detail(request, pregunta_id):
-    return HttpResponse("Estas viendo la pregunta %s." % pregunta_id)
+    preg = get_object_or_404(pregunta, pk=pregunta_id)
+    return render(request, "polls/detail.html", {"pregunta": preg})
 
 def vote(request, pregunta_id):
     return HttpResponse("Estas votando en la pregunta %s." % pregunta_id)

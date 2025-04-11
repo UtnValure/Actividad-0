d6514a0369de30626a7c31c03f7b634c71372696
UtnValure <vlaure@alu.frlp.utn.edu.ar>
2025-04-11 16:25:00 -0300

    [Parte 3.2: Vistas funcionales]

--- DjangoProject/polls/views.py
+++ DjangoProject/polls/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import pregunta
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hola mundo, esta es la vista de Index Polls.")
+    ultimas_preguntas = pregunta.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"ultimas_preguntas": ultimas_preguntas}
+    return HttpResponse(template.render(context, request))
     
 def results(request, pregunta_id):
     response = "Estas viendo los resultados de la pregunta %s."
     return HttpResponse(response % pregunta_id)
 
-def detail(request, pregunta_is):
+def detail(request, pregunta_id):
     return HttpResponse("Estas viendo la pregunta %s." % pregunta_id)
 
 def vote(request, pregunta_id):
